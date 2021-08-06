3e703aeff0e32bee1bb35c4d9e704ae2b79e6c25
mskaff <mskaff904@gmail.com>
2021-08-06 11:01:58 -0400

    test commit

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -22,10 +22,19 @@ router.get('/', async (req, res) => {
 
         // Pass serialized data and session flag into template
         res.render('homepage', {
-            reviews,
-            logged_in: req.session.logged_in
+            reviews
         });
     } catch (err) {
         res.status(500).json(err);
     }
+});
+
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/userprofile');
+        return;
+    }
+
+    res.render('login', { layout: "profile.handlebars" });
 });
