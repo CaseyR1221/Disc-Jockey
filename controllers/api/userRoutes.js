6c9bfdfa3cd094de3bd9fac59ca20032ecb89b87
KhaledLakehal <khaled.lakehal93@gmail.com>
2021-08-06 09:32:25 -0400

    added the review routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const { Reviews } = require('../../../../project-2/Disc-Jockey/models');
 const { User } = require('../../models');
+const withAuth = require('../utils/auth');
 
-router.post('/', async (req, res) => {
+
+router.post('/',withAuth, async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
@@ -59,3 +62,44 @@ router.post('/logout', (req, res) => {
 });
 
 module.exports = router;
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Reviews }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('userprofile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/disc/:id', async (req, res) => {
+  try {
+    const projectData = await Project.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ['name'],
+        },
+      ],
+    });
+
+    const project = projectData.get({ plain: true });
+
+    res.render('Disc', {
+      ...project,
+      logged_in: req.session.logged_in
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
