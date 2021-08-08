779b4f3203f8eb8326100eeda3972a0f8d2c4a2d
mskaff <mskaff904@gmail.com>
2021-08-08 10:48:56 -0400

    created seed data for users and reviews

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,15 +1,25 @@
 const sequelize = require('../config/connection');
-const { Disc } = require('../models');
+const { Disc, User, Reviews } = require('../models');
 
 const discData = require('./discData.json');
+const userData = require('./userData.json');
+const reviewData = require('./reviewData.json')
 
 const seedDatabase = async () => {
   try {
     await sequelize.sync({ force: true });
 
-    await Disc.bulkCreate(discData, {
-      individualHooks: true
+    await Disc.bulkCreate(discData);
+    console.log('\n----- DISCS SYNCED -----\n');
+
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
     });
+    console.log('\n----- USERS SYNCED -----\n');
+    
+    await Reviews.bulkCreate(reviewData);
+    console.log('\n----- REVIEWS SYNCED -----\n');
 
     process.exit(0);
   } catch (err) {
@@ -19,3 +29,4 @@ const seedDatabase = async () => {
 };
 
 seedDatabase();
+
